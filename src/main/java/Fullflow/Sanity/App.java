7aca89bb21a39cfaac8e5e4cf4fb8652a04e1521
gayathrikk <gayathri@htic.iitm.ac.in>
2023-10-18 12:56:30 +0530

    first commit

---
+++ src/main/java/Fullflow/Sanity/App.java
@@ -0,0 +1,13 @@
+package Fullflow.Sanity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
