22b3a95250a8ab60673071cd0c2673ac764f3876
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-19 09:54:28 +0530

    Testing.java

--- src/test/java/Fullflow/Sanity/Testing.java
+++ src/test/java/Fullflow/Sanity/Testing.java
@@ -66,10 +66,10 @@ public class Testing {
         
         
         // Sudha Gopalakrishnan Brain Centre 
-        validateElement("//h1[@class='head']", "Sudha Gopalakrishnan Brain Centre");
+        validateElement("//h1[@class='head m-0']", "Sudha Gopalakrishnan Brain Centre");
         
      // Indian Institute of Technology Madras heading
-        validateElement("(//h1[@class='head']//following::h5)[1]", "Indian Institute of Technology Madras");
+        validateElement("(//h1[@class='head m-0']//following::h5)[1]", "Indian Institute of Technology Madras");
 
         // Home option
         validateElement("(//a[text()='HOME'])[1]", "HOME");
